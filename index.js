da0aff676aaa0f4b201138d66d34695867cefb88
Nathan Stryker <NPSConsultingLLC@gmail.com>
2016-04-15 14:01:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,11 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
+  emailAdapter: SimpleMailgunAdapter({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+    fromAddress: process.env.MAIL_FROM_ADDRESS
+  })
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
